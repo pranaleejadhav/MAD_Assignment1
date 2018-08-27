9b197683df032d15d0dff3044c61411e5f8119ff
lee0392 <pranalee@pranalees-macbook-pro.local>
2018-08-27 12:38:27 -0400

    Initial Commit

---
+++ MAD_Assignment1Tests/MAD_Assignment1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MAD_Assignment1Tests.swift
+//  MAD_Assignment1Tests
+//
+//  Created by Pranalee Jadhav on 8/27/18.
+//  Copyright © 2018 Pranalee Jadhav. All rights reserved.
+//
+
+import XCTest
+@testable import MAD_Assignment1
+
+class MAD_Assignment1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
